49cad93c8c39df3d917a51be0cbf919c60bb31ad
gokalpcevik <cevikgokalp8@gmail.com>
2022-12-15 16:54:34 +0300

    Add functionality according to lab work week 2

--- LoginWindow.py
+++ LoginWindow.py
@@ -3,9 +3,14 @@
     @author: Ali Can Doğan, 120200068
     @author: Ahmed Ibrahim Attya Hamouda, 120200006
     @author: Çağatay Yaman, 120200029
+
+    Stage: Development-02
+    @author: Gökalp Çevik, 120202074
+    @author: Deniz Kuzey, 119202077
 """ 
 
 import tkinter as tk
+from tkinter import ttk
 
 
 class LoginWindow:
@@ -95,11 +100,14 @@ class LoginWindow:
             self.window.destroy()
         
 
+
+
 class NewPage:
     #constructor 
     def __init__(self):
         self.window = tk.Tk() # root window, if you wanna destroy the frame, use this variable
-        self.window.title("New Page")
+        self.window.title("Library")
+        self.window.geometry("640x480")
 
         self._initializeGUI()
         self._addGUIElementsToFrame()
@@ -107,11 +115,43 @@ class NewPage:
         # start the GUI frame
         self.window.mainloop()
     
+
+    # This exceeds 15 lines because combobox inputs are all on seperate lines. 
     def _initializeGUI(self):
         self.lblLoggedin = tk.Label(text="Successfully logged in") 
+        self.bookNameEntry = tk.Entry()
+        self.bookNameTxt = tk.Label(text="Enter Book Name or \n Author Name")
+        self.categoryTxt = tk.Label(text="Category")
+        
+        self.n = tk.StringVar()
+        self.cat_choosen = ttk.Combobox(self.window, width=27,textvariable=self.n)
+        self.cat_choosen['values'] = (' Classics', 
+                          ' Historical Fiction',
+                          ' Horrror',
+                          ' Humour',
+                          ' Poetry',
+                          ' Romance',
+                          ' Science Fiction',
+                          ' Biography',
+                          ' Non-fiction Novel',
+                          ' Crime',
+                          ' Self-help',
+                          ' War')
+        self.cat_choosen.current(0)
+        self.searchBookBtn = tk.Button(text="Search")
+        self.searchBookBtn.bind("<Button-1>", self.handle_click)
 
     def _addGUIElementsToFrame(self):
         self.lblLoggedin.grid(row=0, column=0, padx=10, pady=5)
+        self.bookNameTxt.grid(row=1,column=0,padx=5,pady=5)
+        self.bookNameEntry.grid(row=1,column=1,padx=5,pady=5)
+        self.searchBookBtn.grid(row=3,column=0,padx=5,pady=5)
+        self.cat_choosen.grid(row=2,column=1,padx=5,pady=5)
+        self.categoryTxt.grid(row=2,column=0,padx=5,pady=5)
+
+    def handle_click(self, event):
+        print('Now searching book with name ' + self.bookNameEntry.get() + ' with category ' + self.cat_choosen.get())
+        pass
 
 
 
